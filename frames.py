d7a3c00b0b4d894e864fdf09e228c63e9a1b181f
danlindleybaker <danlindleybaker@gmail.com>
2023-01-06 08:49:59 +0000

    made variable frames homogeneous

--- frames.py
+++ frames.py
@@ -76,14 +76,14 @@ class ValueSelectorWindow(QWidget):
         
         if self.logspace:
             val_list = [f"{x:.2f}" for x in list(np.logspace(
-                np.log10(min_val), np.log10(max_val), points))]
+                np.log10(min_val), np.log10(max_val), int(points)))]
         else:
             if self.combo.currentText() == "Number of Points":
                 val_list = [f"{x:.2f}" for x in list(np.linspace(
-                    min_val, max_val, points))]
+                    min_val, max_val, int(points)))]
             else: 
                 val_list = [f"{x:.2f}" for x in list(np.arange(
-                    min_val, max_val, points))]
+                    min_val, max_val+points, points))]
         
         for val in val_list:
             addValuesToList(self.value_list,val)
@@ -187,123 +187,72 @@ def measurementSettingsFrame(window) -> None:
     layout.addWidget(window.bias_voltage_selector, 0, 5)
 
 
+def populateVariableFrame(window: QMainWindow,frame: QGroupBox, list_box: QListWidget,default_val: float,  min_val: float, max_val: float, logspace: bool = True) -> QGridLayout:
+    layout = QGridLayout(frame)
+
+    frame.setFrame = True
+
+    list_box = QListWidget()
+    list_box.setFixedWidth(150)
+    layout.addWidget(list_box, 0, 0, 4 ,1 )
+    list_box.setSelectionMode(QAbstractItemView.ExtendedSelection)
+    QListWidgetItem(f"{default_val}",list_box)
+
+    add_edit = QLineEdit(f"{default_val}")
+    add_edit.setFixedWidth(100)
+    layout.addWidget(add_edit, 0, 1)
+    add_edit.editingFinished.connect(
+            lambda: limits(window, add_edit, min_val, min_val, False))
+    add_edit.editingFinished.connect(
+            lambda: limits(window, add_edit, max_val, min_val))
+
+    add_button = QPushButton("Add")
+    add_button.setFixedWidth(100)
+    layout.addWidget(add_button, 1, 1)
+    add_button.clicked.connect(lambda: addValuesToList(list_box, add_edit.text()))
+    
+    multi_button  = QPushButton("Add range")
+    multi_button.setFixedWidth(100)
+    layout.addWidget(multi_button, 2, 1)
+    multi_button.clicked.connect(lambda: createMultiValueWindow(window, list_box, min_val, max_val, logspace))
+    
+    delete_button = QPushButton("Delete")
+    delete_button.setFixedWidth(100)
+    layout.addWidget(delete_button, 3, 1)
+    delete_button.clicked.connect(lambda: removeValuesFromList(list_box))
+    
+    return layout
+
 def frequencySettingsFrame(window) -> None:
     window.freq_settings_frame = QGroupBox()
-    layout = QGridLayout(window.freq_settings_frame)
-
-    window.freq_settings_frame.setFrame = True
     window.freq_settings_frame.setTitle("Frequency List")
-
     window.freq_list_widget = QListWidget()
-    layout.addWidget(window.freq_list_widget, 0, 0, 4 ,1 )
-    window.freq_list_widget.setSelectionMode(QAbstractItemView.ExtendedSelection)
-    QListWidgetItem("20",window.freq_list_widget)
 
-    add_freq_edit = QLineEdit("20")
-    layout.addWidget(add_freq_edit, 0, 1)
-    add_freq_edit.editingFinished.connect(
-            lambda: limits(window, add_freq_edit, 20, 20, False))
-    add_freq_edit.editingFinished.connect(
-            lambda: limits(window, add_freq_edit, 2e6, 20))
-
-
-    add_freq_button = QPushButton("Add")
-    layout.addWidget(add_freq_button, 1, 1)
-    add_freq_button.clicked.connect(lambda: addValuesToList(window.freq_list_widget, add_freq_edit.text()))
-    
-    delete_freq_button = QPushButton("Delete")
-    layout.addWidget(delete_freq_button, 2, 1)
-    delete_freq_button.clicked.connect(lambda: removeValuesFromList(window.freq_list_widget))
-
-    multi_freq_button  = QPushButton("Add range")
-    layout.addWidget(multi_freq_button, 3, 1)
-    multi_freq_button.clicked.connect(lambda: createMultiValueWindow(window, window.freq_list_widget, 20, 2e6))
+    populateVariableFrame(window,window.freq_settings_frame, window.freq_list_widget, 20,  20 , 2e6)
 
 
 def voltageSettingsFrame(window) -> None:
     window.voltage_settings_frame = QGroupBox()
-    layout = QGridLayout(window.voltage_settings_frame)
-    window.voltage_settings_frame.setFrame = True
     window.voltage_settings_frame.setTitle("Voltage List")
-
-    
     window.volt_list_widget = QListWidget()
-    layout.addWidget(window.volt_list_widget, 0, 0, 4 ,1 )
-    window.volt_list_widget.setSelectionMode(QAbstractItemView.ExtendedSelection)
-    QListWidgetItem("1",window.volt_list_widget)
 
-    add_volt_edit = QLineEdit("1")
-    layout.addWidget(add_volt_edit, 0, 1)
-    add_volt_edit.editingFinished.connect(
-            lambda: limits(window, add_volt_edit, 20, 20, False))
-    add_volt_edit.editingFinished.connect(
-            lambda: limits(window, add_volt_edit, 2e6, 20))
-
-
-    add_volt_button = QPushButton("Add")
-    layout.addWidget(add_volt_button, 1, 1)
-    add_volt_button.clicked.connect(lambda: addValuesToList(window.volt_list_widget, add_volt_edit.text()))
-    
-    delete_volt_button = QPushButton("Delete")
-    layout.addWidget(delete_volt_button, 2, 1)
-    delete_volt_button.clicked.connect(lambda: removeValuesFromList(window.volt_list_widget))
-
-    multi_volt_button  = QPushButton("Add range")
-    layout.addWidget(multi_volt_button, 3, 1)
-    multi_volt_button.clicked.connect(lambda: createMultiValueWindow(window, window.volt_list_widget, 0, 20, False))
-
+    populateVariableFrame(window,window.voltage_settings_frame, window.freq_list_widget, 1,  0 , 20, False)
 
 
 def temperatureSettingsFrame(window) -> None:
     window.temperature_settings_frame = QGroupBox()
-    layout = QGridLayout(window.temperature_settings_frame)
-    window.temperature_settings_frame.setFrame = True
     window.temperature_settings_frame.setTitle("Temperature List (°C)")
-
-
     window.temp_list_widget = QListWidget()
-    layout.addWidget(window.temp_list_widget, 0, 0, 4 ,1 )
-    window.temp_list_widget.setSelectionMode(QAbstractItemView.ExtendedSelection)
-    QListWidgetItem("25",window.temp_list_widget)
-
-    add_temp_edit = QLineEdit("25")
-    layout.addWidget(add_temp_edit, 0, 1)
-    add_temp_edit.editingFinished.connect(
-            lambda: limits(window, add_temp_edit, -40, -40, False))
-    add_temp_edit.editingFinished.connect(
-            lambda: limits(window, add_temp_edit, 150, -40))
-
-
-    add_temp_button = QPushButton("Add")
-    layout.addWidget(add_temp_button, 1, 1)
-    add_temp_button.clicked.connect(lambda: addValuesToList(window.temp_list_widget, add_temp_edit.text()))
     
-    delete_temp_button = QPushButton("Delete")
-    layout.addWidget(delete_temp_button, 2, 1)
-    delete_temp_button.clicked.connect(lambda: removeValuesFromList(window.temp_list_widget))
+    layout =  populateVariableFrame(window,window.temperature_settings_frame, window.freq_list_widget, 25,  -40 , 150, False)
 
-    multi_temp_button  = QPushButton("Add range")
-    layout.addWidget(multi_temp_button, 3, 1)
-    multi_temp_button.clicked.connect(lambda: createMultiValueWindow(window, window.temp_list_widget, -40, 150, False))
-
-    # layout.addWidget(QLabel("Start Temp."), 0, 0)
-    # window.temp_start = QLineEdit("25")
-    # layout.addWidget(window.temp_start, 1, 0)
-
-    # layout.addWidget(QLabel("End Temp."), 2, 0)
-    # window.temp_end = QLineEdit("30")
-    # layout.addWidget(window.temp_end, 3, 0)
-
-    # layout.addWidget(QLabel("Temp Step."), 4, 0)
-    # window.temp_step = QLineEdit("1")
-    # layout.addWidget(window.temp_step, 5, 0)
-    layout.addWidget(QLabel("Rate."), 4, 0)
+    layout.addWidget(QLabel("Rate (°C/min)"), 0, 2)
     window.temp_rate = QLineEdit("10")
-    layout.addWidget(window.temp_rate, 4, 1)
+    layout.addWidget(window.temp_rate, 0, 3)
 
-    layout.addWidget(QLabel("Stab. Time (s)"), 5, 0)
+    layout.addWidget(QLabel("Stab. Time (s)"), 1, 2)
     window.stab_time = QLineEdit("1")
-    layout.addWidget(window.stab_time, 5, 1)
+    layout.addWidget(window.stab_time, 1, 3)
 
 
 def outputDataSettingsFrame(window) -> None:
