c83e9b33c6749f6142306b6b71bbba3c1d4a3d9a
danlindleybaker <danlindleybaker@gmail.com>
2023-01-26 10:21:08 +0000

    fixed init bug

--- Instruments.py
+++ Instruments.py
@@ -14,6 +14,7 @@ class LinkamHotstage():
         rm = pyvisa.ResourceManager()
 
         self.link = rm.open_resource(self.address)
+        self.init = False
 
         self.link.baud_rate = 19200
 
