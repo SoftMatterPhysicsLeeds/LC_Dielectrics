2f901c87fbd6fb0746d8cbea2f568c62248d0fdf
danlindleybaker <danlindleybaker@gmail.com>
2023-05-26 09:23:59 +0100

    fixed excel writer.. .?

--- lcdielectrics/excel_writer.py
+++ lcdielectrics/excel_writer.py
@@ -1,26 +1,40 @@
 import pandas as pd
 
 
-def make_excel(results: dict, output: str) -> None:
+def make_excel(results: dict, output: str, single_volt: bool) -> None:
 
     with pd.ExcelWriter(output.split(".json")[0] + ".xlsx") as writer:
 
-        
-        for key in results.keys():
+        if single_volt:
+            for key in results.keys():
+                for i, freq in enumerate(results[key].keys()):
+                    df = pd.DataFrame(results[key][freq])
+                    df["freq"] = freq
+                    
+                    cols = list(df.columns.values)
+                    cols.insert(0, cols.pop(cols.index("freq")))
+                    df = df[cols]
+                    if i == 0: 
+                        df.to_excel(writer, sheet_name = str(key), index = False, startrow = i, startcol = 0 )
+                    else:
+                        df.to_excel(writer, sheet_name = str(key), header = False,  index = False, startrow = i+1, startcol = 0)
 
-            for i, freq in enumerate(results[key].keys()):
-                df = pd.DataFrame(results[key][freq])
-                cols = list(df.columns.values)
-                cols.insert(0, cols.pop(cols.index("volt")))
-                df = df[cols]
-                df.to_excel(
-                    writer,
-                    sheet_name=str(key),
-                    index=False,
-                    startrow=((len(df.index) + 3) * i) + 1,
-                    startcol=0,
-                )
-                worksheet = writer.sheets[str(key)]
-                worksheet.write((len(df.index) + 3) * i, 0, "Frequency (Hz): ")
-                worksheet.write((len(df.index) + 3) * i, 1, float(freq))
+        else:
+        
+            for key in results.keys():
+                for i, freq in enumerate(results[key].keys()):
+                    df = pd.DataFrame(results[key][freq])
+                    cols = list(df.columns.values)
+                    cols.insert(0, cols.pop(cols.index("volt")))
+                    df = df[cols]
+                    df.to_excel(
+                        writer,
+                        sheet_name=str(key),
+                        index=False,
+                        startrow=((len(df.index) + 3) * i) + 1,
+                        startcol=0,
+                    )
+                    worksheet = writer.sheets[str(key)]
+                    worksheet.write((len(df.index) + 3) * i, 0, "Frequency (Hz): ")
+                    worksheet.write((len(df.index) + 3) * i, 1, float(freq))
        
