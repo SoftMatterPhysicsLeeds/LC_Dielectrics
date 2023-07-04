1408a7d30b3b67a67fca7931c958305a1885a1fb
danlindleybaker <danlindleybaker@gmail.com>
2023-07-04 15:02:19 +0100

    added 'add' to listboxes

--- lcdielectrics/dpg_ui.py
+++ lcdielectrics/dpg_ui.py
@@ -1,4 +1,13 @@
 import dearpygui.dearpygui as dpg
+from dataclasses import dataclass
+
+@dataclass
+class variable_list:
+    list_handle: str | int
+    add_text_handle: str | int
+    add_button_handle: str | int
+    add_range_handle: str | int
+    del_button_handle: str | int
 
 class lcd_ui():
     def __init__(self):
@@ -8,7 +17,7 @@ class lcd_ui():
         self._make_control_window()
 
     def _make_control_window(self):
-        with dpg.window(label = "Status", pos = [0,0], width = 600) as self.control_window: 
+        with dpg.window(label = "Status", pos = [0,0], width = 600, no_resize=True, no_collapse= True, no_close = True, no_move = True) as self.control_window: 
 
             with dpg.group(tag = "status_window"):
                 self.measurement_status = dpg.add_text(f"{self.status}", tag = "status_display")
@@ -23,7 +32,7 @@ class lcd_ui():
                     self.agilent_com_selector = dpg.add_combo(items = ["USB1"], width = 200)
                     self.agilent_initialise = dpg.add_button(label = "Initialise")
 
-            with dpg.window(label="Measurement Settings",pos= [0, 100]  ,width = 600):
+            with dpg.window(label="Measurement Settings",pos= [0, 100]  ,width = 600, no_resize=True, no_collapse= True, no_close = True, no_move = True):
                 with dpg.group(horizontal=True):
                     dpg.add_text("Delay time (s): ")
                     self.delay_time = dpg.add_input_double(default_value=0.5, width = 100)
@@ -37,15 +46,15 @@ class lcd_ui():
                     dpg.add_text("Bias Level (V)")
                     self.bias_level = dpg.add_combo([0, 1.5, 2], width = 100)
 
-            with dpg.window(label = "Frequency List", width = 300, pos = [0, 200]):
-                self.freq_list, self.add_freq_text, self.freq_add, self.freq_add_range, self.freq_delete = make_variable_list_frame(20)
+            with dpg.window(label = "Frequency List", width = 300, pos = [0, 200], no_resize=True, no_collapse= True, no_close = True, no_move = True):
+                self.freq_list = variable_list(*make_variable_list_frame(20.0))
             
-            with dpg.window(label = "Voltage List", width = 300, pos = [300, 200]):
-                self.volt_list, self.add_volt_text, self.volt_add, self.volt_add_range, self.volt_delete = make_variable_list_frame(1)
+            with dpg.window(label = "Voltage List", width = 300, pos = [300, 200], no_resize=True, no_collapse= True, no_close = True, no_move = True):
+                self.volt_list = variable_list(*make_variable_list_frame(1.0))
 
-            with dpg.window(label = "Temperature List", width = 600, pos = [0, 323]):
+            with dpg.window(label = "Temperature List", width = 600, pos = [0, 323], no_resize=True, no_collapse= True, no_close = True, no_move = True):
                 with dpg.group(horizontal=True):
-                    self.volt_list, self.add_volt_text, self.volt_add, self.volt_add_range, self.volt_delete = make_variable_list_frame(25)
+                    self.temperature_list =  variable_list(*make_variable_list_frame(25.0))
                     with dpg.group():
                         with dpg.group(horizontal=True):
                             self.go_to_temp_button = dpg.add_button(label = "Go to:")
@@ -59,7 +68,7 @@ class lcd_ui():
                             dpg.add_input_double(default_value=1)
 
 
-            with dpg.window(label = "Output Data Settings", pos = [0, 446], width = 600):
+            with dpg.window(label = "Output Data Settings", pos = [0, 446], width = 600, no_resize=True, no_collapse= True, no_close = True, no_move = True):
                 with dpg.group(horizontal=True):
                     self.output_file_path  = dpg.add_input_text(default_value="results.json")
                     self.browse_button = dpg.add_button(label = "Browse", callback = lambda: dpg.show_item("file_dialog_id"))
@@ -69,18 +78,24 @@ class lcd_ui():
             
 
 def file_saveas_callback(sender, app_data, output_file_path):
-    # print("Sender: ", sender)
-    # print("App Data: ", app_data)
     dpg.set_value(output_file_path, app_data["file_path_name"])
 
+def add_value_to_list_callback(sender, app_data, user_data):
+    current_list = dpg.get_item_configuration(user_data["listbox_handle"])["items"]
+    
+    current_list.append(str(dpg.get_value(user_data["add_text"])))
+    dpg.configure_item(user_data["listbox_handle"],items = current_list )
 
-            
+def del_value_from_list_callback(sender, app_data, user_data):
+    pass
+
+
 def make_variable_list_frame(default_val):
     with dpg.group(horizontal=True):
-        listbox_handle = dpg.add_listbox([default_val], width = 200)
+        listbox_handle = dpg.add_listbox(["1:\t" + str(default_val)], width = 200)
         with dpg.group():
             add_text = dpg.add_input_float(default_value = default_val, width = 100)
-            add_button = dpg.add_button(label = "Add")
+            add_button = dpg.add_button(label = "Add",  callback=add_value_to_list_callback, user_data={"listbox_handle": listbox_handle, "add_text": add_text})
             add_range_button = dpg.add_button(label = "Add Range")
             delete_button = dpg.add_button(label = "Delete")
         
