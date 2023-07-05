2b6bcd1840226b1bc811d3954ed6872417315c6f
danlindleybaker <danlindleybaker@gmail.com>
2023-07-05 16:00:04 +0100

    added aiblity to list connected USB devices

--- lcdielectrics/main.py
+++ lcdielectrics/main.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 from lcdielectrics.dpg_ui import lcd_ui
 from serial.tools import list_ports
+import pyvisa
 
 def main():
     dpg.create_context()
@@ -14,9 +15,13 @@ def main():
     frontend = lcd_ui()
 
     com_selector = [x.__str__() for x  in list_ports.comports()]
-    
+    rm = pyvisa.ResourceManager()
+    visa_resources = rm.list_resources()
+
+    usb_selector = [x for x in visa_resources if x.split("::")[0][0:3] == "USB"]
+
     dpg.configure_item(frontend.linkam_com_selector, items = com_selector)
-    # dpg.configure_item(frontend.agilent_com_selector, items = usb_selector)
+    dpg.configure_item(frontend.agilent_com_selector, items = usb_selector)
     dpg.show_item_registry()
 
 
