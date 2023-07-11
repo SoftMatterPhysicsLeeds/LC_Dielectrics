337f453f86cd8894fa10f0353b238a86e956491b
danlindleybaker <danlindleybaker@gmail.com>
2023-07-11 09:32:57 +0100

    added instruments dataclass

--- lcdielectrics/main.py
+++ lcdielectrics/main.py
@@ -1,12 +1,14 @@
-from lcdielectrics.lcd_utils import find_instruments
+from lcdielectrics.lcd_utils import find_instruments, lcd_instruments
 import dearpygui.dearpygui as dpg
 from lcdielectrics.lcd_ui import lcd_ui, VIEWPORT_HEIGHT, VIEWPORT_WIDTH, DRAW_HEIGHT
+
 # from serial.tools import list_ports
-import threading 
+import threading
 import ctypes
+
 ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
-#TODO: hook up UI to functions that actually run the experiment. State class perhaps? 
+# TODO: hook up UI to functions that actually run the experiment. State class perhaps?
 FONT_SCALE = 1
 
 
@@ -14,38 +16,32 @@ def main():
     dpg.create_context()
 
     with dpg.font_registry():
-        font_regular = dpg.add_font('consola.ttf', 14*FONT_SCALE)
-    
-    dpg.set_global_font_scale(1/FONT_SCALE)    
+        font_regular = dpg.add_font("consola.ttf", 14 * FONT_SCALE)
+
+    dpg.set_global_font_scale(1 / FONT_SCALE)
     dpg.bind_font(font_regular)
 
-
-
     dpg.create_viewport(
-        title = "LC Dielectrics",
-        width = VIEWPORT_WIDTH,
-        height= DRAW_HEIGHT
-    
+        title="LC Dielectrics", width=VIEWPORT_WIDTH, height=DRAW_HEIGHT
     )
     dpg.setup_dearpygui()
     dpg.show_viewport()
 
     frontend = lcd_ui()
+    instruments = lcd_instruments()
 
-
-    #dpg.show_item_registry()
+    # dpg.show_item_registry()
 
     # Search for instruments using a thread so GUI isn't blocked.
-    thread = threading.Thread(target=find_instruments, args = (frontend,))
+    thread = threading.Thread(target=find_instruments, args=(frontend,))
     thread.daemon = True
     thread.start()
 
-
     while dpg.is_dearpygui_running():
-
         dpg.render_dearpygui_frame()
 
     dpg.destroy_context()
+
 
 if __name__ == "__main__":
     main()
