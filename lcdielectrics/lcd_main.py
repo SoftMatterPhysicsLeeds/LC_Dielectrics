c4cc17d8a670a4fb123813a3e843fdab083cbc7e
danlindleybaker <danlindleybaker@gmail.com>
2023-11-24 08:14:31 +0000

    refactored measurement status handling

--- lcdielectrics/lcd_main.py
+++ lcdielectrics/lcd_main.py
@@ -4,6 +4,7 @@ from lcdielectrics.lcd_utils import (
     lcd_state,
     read_temperature,
     run_spectrometer,
+    handle_measurement_status
 )
 from lcdielectrics.lcd_themes import generate_global_theme
 import dearpygui.dearpygui as dpg
@@ -42,56 +43,12 @@ def main():
 
     while dpg.is_dearpygui_running():
         # check if linkam is connected. If it is, start thread to poll temperature.
-        current_wait = 0
+        
         if state.linkam_connection_status == "Connected":
             linkam_thread.start()
             state.linkam_connection_status = "Reading"
 
-        if state.measurement_status == Status.IDLE or Status.COLLECTING_DATA:
-            pass
-        elif state.measurement_status == Status.SET_TEMPERATURE and (
-            state.linkam_action == "Stopped" or state.linkam_action == "Holding"
-        ):
-            instruments.linkam.set_temperature(
-                state.T_list[state.T_step], dpg.get_value(frontend.T_rate)
-            )
-            state.measurement_status = Status.GOING_TO_TEMPERATURE
-            state.measurement_status = f"Going to T: {state.T_list[state.T_step]}"
-
-        elif (
-            state.measurement_status == Status.GOING_TO_TEMPERATURE
-            and state.linkam_action == "Holding"
-        ):
-            state.t_stable_start = time.time()
-            state.measurement_status = Status.STABILISING_TEMPERATURE
-
-        elif Status.STABILISING_TEMPERATURE:
-            current_wait = time.time() - state.t_stable_start
-            dpg.set_value(
-                frontend.measurement_status,
-                f"Waiting for {current_wait}/{dpg.get_value(frontend.stab_time)}s",
-            )
-            if current_wait >= dpg.get_value(frontend.stab_time):
-                state.measurement_status = Status.TEMPERATURE_STABILISED
-
-        elif state.measurement_status == Status.TEMPERATURE_STABILISED:
-            state.measurement_status = Status.COLLECTING_DATA
-            instruments.agilent.set_frequency(state.freq_list[state.freq_step])
-            instruments.agilent.set_voltage(state.voltage_list[state.volt_step])
-
-            run_spectrometer(frontend, instruments, state)
-
-        elif state.measurement_status == Status.COLLECTING_DATA:
-            dpg.set_value(
-                frontend.measurement_status,
-                f"Measuring f = {state.freq_list[state.freq_step]}, V = {state.voltage_list[state.volt_step]}",
-            )
-
-        elif state.measurement_status == Status.FINISHED:
-            instruments.linkam.stop()
-            instruments.agilent.reset_and_clear()
-            state.measurement_status = Status.IDLE
-            dpg.set_value(frontend.measurement_status, "Idle")
+        handle_measurement_status(state, frontend, instruments)
 
 
         dpg.render_dearpygui_frame()
--- lcdielectrics/lcd_utils.py
+++ lcdielectrics/lcd_utils.py
@@ -10,6 +10,55 @@ import threading
 # TODO: find a way to handle exceptions in instrument threads?
 
 
+def handle_measurement_status(state: lcd_state, frontend: lcd_ui, instruments: lcd_instruments):
+    current_wait = 0
+    if state.measurement_status == Status.IDLE or Status.COLLECTING_DATA:
+        pass
+    elif state.measurement_status == Status.SET_TEMPERATURE and (
+        state.linkam_action == "Stopped" or state.linkam_action == "Holding"
+    ):
+        instruments.linkam.set_temperature(
+            state.T_list[state.T_step], dpg.get_value(frontend.T_rate)
+        )
+        state.measurement_status = Status.GOING_TO_TEMPERATURE
+        state.measurement_status = f"Going to T: {state.T_list[state.T_step]}"
+
+    elif (
+        state.measurement_status == Status.GOING_TO_TEMPERATURE
+        and state.linkam_action == "Holding"
+    ):
+        state.t_stable_start = time.time()
+        state.measurement_status = Status.STABILISING_TEMPERATURE
+
+    elif Status.STABILISING_TEMPERATURE:
+        current_wait = time.time() - state.t_stable_start
+        dpg.set_value(
+            frontend.measurement_status,
+            f"Waiting for {current_wait}/{dpg.get_value(frontend.stab_time)}s",
+        )
+        if current_wait >= dpg.get_value(frontend.stab_time):
+            state.measurement_status = Status.TEMPERATURE_STABILISED
+
+    elif state.measurement_status == Status.TEMPERATURE_STABILISED:
+        state.measurement_status = Status.COLLECTING_DATA
+        instruments.agilent.set_frequency(state.freq_list[state.freq_step])
+        instruments.agilent.set_voltage(state.voltage_list[state.volt_step])
+
+        run_spectrometer(frontend, instruments, state)
+
+    elif state.measurement_status == Status.COLLECTING_DATA:
+        dpg.set_value(
+            frontend.measurement_status,
+            f"Measuring f = {state.freq_list[state.freq_step]}, V = {state.voltage_list[state.volt_step]}",
+        )
+
+    elif state.measurement_status == Status.FINISHED:
+        instruments.linkam.stop()
+        instruments.agilent.reset_and_clear()
+        state.measurement_status = Status.IDLE
+        dpg.set_value(frontend.measurement_status, "Idle")
+
+
 def find_instruments(frontend: lcd_ui):
     # com_selector = [x.__str__() for x  in list_ports.comports()]
 
@@ -106,7 +155,7 @@ def get_result(
             and state.volt_step == len(state.voltage_list) - 1
             and state.freq_step == len(state.freq_list) - 1
         ):
-            state.measurement_status = Status.FINISHED 
+            state.measurement_status = Status.FINISHED
 
         else:
             if (
