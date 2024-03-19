f556f3ca45b4450b264af0a74b3f404bc5dea257
danlindleybaker <danlindleybaker@gmail.com>
2024-03-19 10:48:37 +0000

    fixing visa writes

--- lcdielectrics/lcd_ui.py
+++ lcdielectrics/lcd_ui.py
@@ -186,6 +186,7 @@ class lcd_ui:
                     self.agilent_initialise = dpg.add_button(
                         label="Initialise", width=-1
                     )
+                    
                 with dpg.table_row():
                     dpg.add_text("Oscilloscope: ")
                     self.oscilloscope_status = dpg.add_text(
@@ -196,9 +197,12 @@ class lcd_ui:
                     self.oscilloscope_initialise = dpg.add_button(
                         label="Initialise", width=-1
                     )
+                    
                 with dpg.table_row():
                     self.num_averages_text = dpg.add_text("N: ", show=False)
                     self.num_averages = dpg.add_input_int(default_value=5, width=-1, step =0, step_fast=0, show=False)
+
+               
             with dpg.window(
                 label="Measurement Settings",
                 no_collapse=True,
--- lcdielectrics/lcd_utils.py
+++ lcdielectrics/lcd_utils.py
@@ -84,27 +84,31 @@ def stop_measurement(instruments: lcd_instruments, state: lcd_state) -> None:
 def init_agilent(
     frontend: lcd_ui, instruments: lcd_instruments, state: lcd_state
 ) -> None:
+    if instruments.agilent:
+        instruments.agilent.close()
     agilent = AgilentSpectrometer(dpg.get_value(frontend.agilent_com_selector))
     dpg.set_value(frontend.agilent_status, "Connected")
-    dpg.hide_item(frontend.agilent_initialise)
+    dpg.configure_item(frontend.agilent_initialise, label = "Reconnect")
     instruments.agilent = agilent
     state.agilent_connection_status = "Connected"
 
 def init_oscilloscope(
     frontend: lcd_ui, instruments: lcd_instruments, state: lcd_state
 ) -> None:
+    if instruments.oscilloscope:
+        instruments.oscilloscope.close()
     rm = pyvisa.ResourceManager()
-    oscilloscope = rm.open_resource(dpg.get_value(frontend.oscilloscope_com_selector))
+    instruments.oscilloscope = rm.open_resource(dpg.get_value(frontend.oscilloscope_com_selector))
+    instruments.oscilloscope.write("*RST; *CLS")
     dpg.set_value(frontend.oscilloscope_status, "Connected")
-    dpg.hide_item(frontend.oscilloscope_initialise)
+    dpg.configure_item(frontend.oscilloscope_initialise, label = "Reconnect")
     dpg.show_item(frontend.num_averages)
     dpg.show_item(frontend.num_averages_text)
-    instruments.oscilloscope = oscilloscope
     state.oscilloscope_connection_status = "Connected"
     # oscilloscope.write(":AUToscale")
-    oscilloscope.write(":WAVeform:FORMat ASCII")
-    oscilloscope.write(":ACQuire:TYPE NORMal")
-    oscilloscope.write(":TIMebase:DELay 0")
+    instruments.oscilloscope.write(":WAVeform:FORMat ASCII")
+    instruments.oscilloscope.write(":ACQuire:TYPE NORMal")
+    instruments.oscilloscope.write(":TIMebase:DELay 0")
 
 def init_linkam(
     frontend: lcd_ui, instruments: lcd_instruments, state: lcd_state
@@ -208,7 +212,10 @@ def handle_measurement_status(
 
     elif state.measurement_status == Status.FINISHED:
         instruments.linkam.stop()
-        instruments.agilent.reset_and_clear()
+        try:
+            instruments.agilent.reset_and_clear()
+        except Exception as e:
+            print("Error initialising the Agilent: ", e)
         state.measurement_status = Status.IDLE
         dpg.set_value(frontend.measurement_status, "Idle")
 
@@ -261,11 +268,19 @@ def get_data_from_scope(frontend: lcd_ui, instruments: lcd_instruments, state: l
     for i in range(n):
         print(f"Measuring {i+1}/{dpg.get_value(frontend.num_averages)} f = {state.freq_list[state.freq_step]:.2f}, V = {state.voltage_list[state.volt_step]} ")
         instruments.oscilloscope.write(":DIGitize CHANnel1")
-        data = instruments.oscilloscope.query(":WAV:DATA?")
+        data = "1"
+        try:    
+            data = instruments.oscilloscope.query(":WAV:DATA?")
+        except Exception as e:
+            print("Data read failed: ", e)
         data = data.strip().split(",")
         data = [float(x) for x in data[1:]]
         average = sum(data) / len(data)
         total.append(average)
+    instruments.oscilloscope.write("*RST; *CLS")
+    instruments.oscilloscope.write(":WAVeform:FORMat ASCII")
+    instruments.oscilloscope.write(":ACQuire:TYPE NORMal")
+    instruments.oscilloscope.write(":TIMebase:DELay 0")
     return total
 
 def read_temperature(frontend: lcd_ui, instruments: lcd_instruments, state: lcd_state):
